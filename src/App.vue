f06b523ecc317f827a28f5cc4b91575bcce26b72
mio-dokuhaki <a214mm040s@std.kurume-u.ac.jp>
2025-03-18 11:43:24 +0900

    JaKooLit Tech World

--- src/App.vue
+++ src/App.vue
@@ -18,10 +18,11 @@
       </div>
     </section>
 
-    <!-- YouTube Video Section -->
+    <!-- YouTube Playlist Section -->
     <section class="youtube-section">
       <div class="youtube-container">
-        <iframe src="https://www.youtube.com/embed/5DEw_CrHgAA" title="JaKooLit Video" frameborder="0" allowfullscreen>
+        <iframe src="https://www.youtube.com/embed/videoseries?list=PLDtGd5Fw5_GjXCznR0BzCJJDIQSZJRbxx"
+          title="JaKooLit YouTube Playlist" frameborder="0" allowfullscreen>
         </iframe>
       </div>
 
